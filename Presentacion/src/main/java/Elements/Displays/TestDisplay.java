18d10cdbc91e895790ac6960cac8ff12f51e08b3
joeljolos-TW <joel.cabe2403@gmail.com>
2026-04-06 14:06:34 -0700

    Inicio de la creacion de la capa de presentacion #6: Creacion de elementos (panel que mostrara las portadas interactivas de las peliculas)

--- Presentacion/src/main/java/Elements/Displays/TestDisplay.java
+++ Presentacion/src/main/java/Elements/Displays/TestDisplay.java
@@ -3,35 +3,20 @@ package Elements.Displays;
 import javax.swing.*;
 import Elements.Buttons.GenericButton;
 import Elements.Panels.MovieCover;
+import Elements.Panels.SwitchPanel;
 
 import java.awt.*;
 
 public class TestDisplay extends JFrame {
 
     private GenericButton boton;
-
+    private SwitchPanel contenedor;
+    private GenericButton newPanelBoton;
     public TestDisplay(String title) {
         super(title);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-        boton = new GenericButton("Click me", false, 10, 120, 40, Color.WHITE, Color.BLUE, Color.CYAN);
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.add(boton);
-
-        MovieCover scottPilgrim = new MovieCover("src/main/resources/images.jpeg","Scott Pilgrim");
-        scottPilgrim.setPreferredSize(new Dimension(200, 300));
-        panel.add(scottPilgrim);
-
-        add(panel);
-
-        setSize(800, 600);
-        setVisible(true);
-        setResizable(false);
-        setLocationRelativeTo(null);
-
-        IO.println(scottPilgrim.getPosterLabel().getIcon().getIconWidth());
+        contenedor = new SwitchPanel();
     }
 }
