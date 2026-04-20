34c15e2c30cce07ef25a190407048e16641f9932
Ricardo Cervantes <149277067+reeckop@users.noreply.github.com>
2026-04-19 23:32:21 -0700

    Las otras capas

---
+++ Servicios/src/main/java/itson/servicios/Servicios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.servicios;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Servicios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
